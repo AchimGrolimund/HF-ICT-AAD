673632f5a469973618497354a5f3bdf4afcb59f3
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-09-29 19:39:09 +0200

    Update

--- Rekursion/main.cpp
+++ Rekursion/main.cpp
@@ -78,38 +78,52 @@ double power(double x, int n) { //x = Base, n = hoch
 	}
 }
 
-int main(int argc, char **argv) {
+int main() {
 
 
-
+	auto start = high_resolution_clock::now();
 	// Test Aufgabe 2
 	if (fibRe(1) == 1 && fibRe(3) == 2 && fibRe(8) == 21) {
 		cout << "Aufgabe 2 ist richtig" << endl;
 	} else {
 		cout << "Aufgabe 2 ist falsch" << endl;
 	}
+	auto ende  = high_resolution_clock::now();
+	cout << endl << "Zeit: " << std::chrono::duration_cast<nanoseconds>(ende-start).count() << " ns"<<endl;
+	cout << "---------------------------------------"<<endl;
 
+	auto start1 = high_resolution_clock::now();
 	// Test Aufgabe 3
 	if (fibIt(1) == 1 && fibIt(3) == 2 && fibIt(8) == 21) {
 		cout << "Aufgabe 3 ist richtig" << endl;
 	} else {
 		cout << "Aufgabe 3 ist falsch" << endl;
 	}
+	auto ende1  = high_resolution_clock::now();
+	cout << endl << "Zeit: " << std::chrono::duration_cast<nanoseconds>(ende1-start1).count() << " ns"<<endl;
+	cout << "---------------------------------------"<<endl;
 
+	auto start2 = high_resolution_clock::now();
 	// Test Aufgabe 4
 	if (fibReImp(1) == 1 && fibReImp(3) == 2 && fibReImp(8) == 21) {
 		cout << "Aufgabe 4 ist richtig" << endl;
 	} else {
 		cout << "Aufgabe 4 ist falsch" << endl;
 	}
+	auto ende2  = high_resolution_clock::now();
+	cout << endl << "Zeit: " << std::chrono::duration_cast<nanoseconds>(ende2-start2).count() << " ns"<<endl;
+	cout << "---------------------------------------"<<endl;
 
+	auto start3 = high_resolution_clock::now();
 	// Test Aufgabe 5
 	if (power(2, 2) == 4 && power(2, 0) == 1 && power(3, 3) == 27) {
 		cout << "Aufgabe 5 ist richtig" << endl;
 	} else {
 		cout << "Aufgabe 5 ist falsch" << endl;
 	}
-
+	auto ende3  = high_resolution_clock::now();
+	cout << endl << "Zeit: " << std::chrono::duration_cast<nanoseconds>(ende3-start3).count() << " ns"<<endl;
+	cout << "---------------------------------------"<<endl;
 	return 0;
 }
 
