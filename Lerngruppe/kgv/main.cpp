e2c1398664c0e486f55303677d9809e516cd8025
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-10-16 17:17:57 +0200

    update

--- Lerngruppe/kgv/main.cpp
+++ Lerngruppe/kgv/main.cpp
@@ -28,13 +28,18 @@ using namespace std;
 int kgv(int,int);
 int ggtRek(int,int);
 
-
 int main()
 {
+
+	auto start = myFunks::myTime::start();
+	std::this_thread::sleep_for (std::chrono::seconds(1));
 	cout<<kgv(12,18)<<endl; //36
+	myFunks::myTime::stop(start);
 
 	return 0;
 }
+
+
 
 int kgv(int zahl1, int zahl2)
 {
