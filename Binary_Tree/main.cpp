b829a1dbcf544cccc67beb2fdcae6b616371e96d
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-10-28 14:17:17 +0200

    Update

--- Binary_Tree/main.cpp
+++ Binary_Tree/main.cpp
@@ -25,71 +25,89 @@
 
 using namespace std;
 
-class Node
-{
-	public:
-		Node * left;
-		Node * right;
-		int value;
-		Node( int value);
+class Node {
+public:
+  Node * left;
+  Node * right;
+  int value;
+  Node(int value);
 };
 
-Node::Node(int value)  : value(value), left(0), right(0){
+Node::Node(int value) : left(0), right(0), value(value) {
 }
 
-
-class BinaryTree
-{
-	private:
-		Node * root;
-		void insert(int v, Node * n);
-	public:
-		BinaryTree();
-		void insert(int v);
-		int height();
+class BinaryTree {
+private:
+  Node * root;
+  void insert(int v, Node * n);
+  void postOrder(Node * n);
+public:
+  BinaryTree();
+  void insert(int v);
+  int height(Node *n, int &ch, int &max);
+  void postOrder();
 };
 
-void BinaryTree::insert(int v, Node *n)
-{
-	if (v < n->value) {
-		//left
-		if (n->left == 0) {
-			n->left = new Node(v);
-		}
-	}else{
-		//right
-		if (n->right == 0) {
-			n->right = new Node(v);
-		}else{
-			insert(v,n->right);
-		}
-	}
+BinaryTree::BinaryTree() : root(0) {
 }
 
-void BinaryTree::insert(int v){
-	if (root == 0) {
-		root = new Node(v);
-	}else{
-		insert(v,root);
+void BinaryTree::insert(int v, Node * n) {
+  if (v < n->value) {
+	// left
+	if (n->left == 0) {
+	  n->left = new Node(v);
+	} else {
+	  insert(v, n->left);
 	}
+  } else {
+	// right
+	if (n->right == 0) {
+	  n->right = new Node(v);
+	} else {
+	  insert(v, n->right);
+	}
+  }
 }
 
-int BinaryTree::height()
+void BinaryTree::postOrder(Node *n)
 {
-
+	if(n != 0){
+		postOrder(n->left);
+		postOrder(n->right);
+		cout<< n->value<<endl;
+	}
+}
+void BinaryTree::postOrder()
+{
+	postOrder(root);
 }
 
+void BinaryTree::insert(int v) {
+  if (root == 0) {
+	root = new Node(v);
+  } else {
+	insert(v, root);
+  }
+}
 
-int main()
+int BinaryTree::height(Node *n, int &ch, int &max)
 {
-	BinaryTree bt;
-	int values[] {50,25,75,14,7,19,63,89,99,79};
-
-	for(int &x : values){
-		bt.insert(x);
-	}
-
 	return 0;
+}
+
+
+
+
+
+int main() {
+  BinaryTree bt;
+  int values[] = {50, 25, 75, 14, 7, 19, 63, 89, 99, 79};
+  for (int &x : values) {
+	bt.insert(x);
+  }
+
+bt.postOrder();
+  return 0;
 }
 
 
