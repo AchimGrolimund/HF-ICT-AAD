6f58fff843c9e8aee570d0b6fb5dc033855c3c34
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-11-10 09:39:50 +0100

    Finish update

--- Sheet 5/Aufgabe_04/main.cpp
+++ Sheet 5/Aufgabe_04/main.cpp
@@ -31,23 +31,23 @@ int calculate(string input){
 	vector<int> asciiBuchstabe(26);
 	int zaehler(0), multiplikator(26);
 
+	// <-- Transform all to lower case -->
 	transform(input.begin(),input.end(),input.begin(), ::tolower);
 
-	for(auto&& i : input){
-		if(i != ' '){
-			asciiBuchstabe[static_cast<unsigned int>(i-'a')] += 1;
-		}
+	// <-- input 1x durchlaufen -->
+	for(const auto &i : input){
+		if(i != ' ') asciiBuchstabe[static_cast<unsigned int>(i-'a')] += 1;
 	}
 
 	sort(asciiBuchstabe.begin(),asciiBuchstabe.end(), std::greater<int>());
 
-	for(auto&& j : asciiBuchstabe){
+	// <--  -->
+	for(const auto &j : asciiBuchstabe){
 		if(j > 0 && multiplikator > 0){
 			zaehler+= (j*multiplikator);
 			multiplikator--;
 		}
 	}
-
 	return zaehler;
 }
 
@@ -61,17 +61,22 @@ int main()
 
 	}
 
-
-	string myString = "aabcdefg hijklmnopqrstuvwxyz";
+	string myString = "abcdefghijklmnopqrstuvwxyz";
+	string myString3 = "Sometimes test cases are hard to make up";
 
 	auto start = myTime::start();
-	cout << "Ausgabe ohne move:\n";
-	cout << "Die schoenheit deiner Zeichenkette ist: "<<calculate(myString)<<endl;
+	cout << "Stringlaenge " << myString.size() << "\n";
+	cout << "Die schoenheit deiner Zeichenkette ist: "<<calculate(move(myString))<<"\n";
 	myTime::stop(start);
 	cout << "---------------------------------\n";
 	start = myTime::start();
-	cout << "Ausgabe mit move:\n";
-	cout<<"Die schoenheit deiner Zeichenkette ist: "<<calculate(move(myString))<<endl;
+	cout << "Stringlaenge " << myString3.size() << "\n";
+	cout<<"Die schoenheit deiner Zeichenkette ist: "<<calculate(move(myString3))<<"\n";
+	myTime::stop(start);
+	cout << "---------------------------------\n";
+	start = myTime::start();
+	cout << "Stringlaenge " << myString2.size() << "\n";
+	cout<<"Die schoenheit deiner Zeichenkette ist: "<<calculate(move(myString2))<<"\n";
 	myTime::stop(start);
 
 	return 0;
