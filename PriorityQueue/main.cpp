3ebd6c11b347bae917aca501546adbda28b1322c
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-11-25 13:20:35 +0100

    Init upload

---
+++ PriorityQueue/main.cpp
@@ -0,0 +1,32 @@
+/*===================================[ Algorithmen und Datenstrukturen]================================================================*
+ *
+
+ *
+ * @Version:		1.0.0
+ * @Author:			Achim Grolimund (achim.grolimund@hf-ict.info)
+ * @Date:			01.1.2017
+ *
+ * @Description:
+
+ *
+ * Beispiel:
+
+ *
+ * Anmerkung:
+ *
+ *
+ *==============================================[ EOF RDM ]=============================================================================*/
+
+#define DEBUG
+#include <bits/stdc++.h>
+
+#include "C:/Users/achim/Documents/Programming/C++/HF-ICT-AAD/myFunks/myFunks/myfunks.h"
+#include "C:/Users/achim/Documents/Programming/C++/HF-ICT-AAD/myFunks/myFunks/debug.h"
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
